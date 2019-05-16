4725650b1fe5da8354167e9e3b670677f96cbcd2
youxiaohua <714643268@qq.com>
2019-05-16 14:51:51 +0800

    new

--- myCalc.js
+++ myCalc.js
@@ -1,6 +1,10 @@
 'use strict'
 
 function myCalc(expr, v){
-	this.s_idle	= 1;
+	this.s_idle		= 1;
 	this.s_string_double	= 2;
+	this.s_string_single	= 3;
+	this.s_number_int	= 4;
+	this.s_number_float	= 5;
+	this.s_name		= 6;
 }
