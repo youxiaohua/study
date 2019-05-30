1c58adee219ddc4136acf8f61a704150b03d9745
youxiaohua <714643268@qq.com>
2019-05-30 18:05:12 +0800

    开始实现单个字符的函数

--- calc.c
+++ calc.c
@@ -3,26 +3,36 @@
 
 stack *init(){
   stack *head =(stack *)malloc(sizeof(stack));
-  //  head->type = NULL;
-  //head->data_type = NULL;
-  //head->token = NULL;
   head->next = NULL;
   return head;
   
 }
 
-void push(char *type,char *data_type,char *token,stack *head){
+void push(int type,int data_type,char *token,stack *head){
   stack *new = (stack *)malloc(sizeof(stack));
-  strcpy(new->type,type);
-  if(data_type != NULL){
-    strcpy(new->data_type , data_type);
+  new->type = type;
+  if(data_type != 0){
+    new->data_type = data_type;
   }
   strcpy(new->token , token);
   new->next = NULL;
   add_list(new,head);  
 }
+void push_stack(stack *p,stack *head){
+  stack *new = (stack *)malloc(sizeof(stack));
+  new->type = p->type;
+  if(p->data_type != 0){
+    new->data_type = p->data_type;
+  }
+  strcpy(new->token , p->token);
+  new->next = NULL;
+  add_list(new,head);
+}
 stack *pop(stack *head){
   stack *tmp = head->next;
+  if(tmp == NULL){
+    return NULL;
+  }
   head->next = tmp->next;
   return tmp;
 }
@@ -57,15 +67,18 @@ void free_stack(stack *head){
 void show(stack *head){
   stack *tmp=head->next;
   while(tmp != NULL){
-    printf("%s\n",tmp->token);
+    printf("%s ",tmp->token);
     tmp = tmp->next;
   }
+  printf("\n");
 }
 struct op *check_oper(char *ch){
   int size = sizeof(oper)/sizeof(struct op);
   int i=0;
+  
   while(i<size){
     if(strcmp(oper[i].ch,ch) == 0){
+      // printf("--[%s]   \n",oper[i].ch);
       return &oper[i];
     }
     i++;
@@ -73,25 +86,34 @@ struct op *check_oper(char *ch){
   return NULL;
 }
 stack *exprs;
-stack *output;
+
 stack *result;
+
 int main(int argc,char **argv){
+  stack *output = init();
+  
   exprs = init();
-  pass1("(1+2)*3");
+  pass1("(2 + 1) - 3 / 2");
+ 
+  reverse(exprs);
   show(exprs);
+  pass2(output);
+  reverse(output);
+  show(output);
+
+  
 }
 
-void pass1(char *expr){
+void pass1(char *ep){
   char ch;
-  printf("读取到%s\n",expr);
+  char ch1[2]={0};
   char token[20] = {0};
-  int len = strlen(expr);
+  int len = strlen(ep);
   int pos=0;
   int num=0;
   int stat = s_idle;
-  while(pos<len){
-    ch = expr[pos];
-    printf("ch = [%c] token = [%s]  num = %d  pos = %d\n",ch,token,num,pos);
+  while(pos<=len){
+    ch = ep[pos];
     switch(stat){
     case s_idle:  
       if(ch == ' ' || ch == '\n'){
@@ -110,28 +132,30 @@ void pass1(char *expr){
 	pos++;
 	stat = s_array;
       }else if(memchr(c_operator_prefix,ch,strlen(c_operator_prefix))){
-	printf("[%c]\n",ch);
 	if(ch == '('){
-	  push("left_brace",NULL,&ch,exprs);
+	  ch1[0]=ch;
+	  push(LEFT_BRACE,0,ch1,exprs);
 	}else if(ch == ')'){
-	  push("right_brace",NULL,&ch,exprs);
+	  ch1[0]=ch;
+	  push(RIGHT_BRACE,0,ch1,exprs);
 	}else{
-	  char ch2[2] = {expr[pos],expr[pos+1]};
-	  printf("------%s\n",ch2);
-	  if(check_oper(&ch)){
-	    printf("%d\n",__LINE__);
-	    push("operator",NULL,&ch,exprs);
+	  ch1[0]=ch;
+	  char ch2[3];
+	  ch2[0]=ch;
+	  ch2[1]=ep[pos+1];
+	  ch2[2]=0;
+	  if(check_oper(ch1)){
+	    push(OPERATOR,0,ch1,exprs);
 	  }else if(check_oper(ch2)){
-	    printf("%d\n",__LINE__);
-	    push("operator",NULL,ch2,exprs);
+	    push(OPERATOR,0,ch2,exprs);
 	    pos++;
 	  }
      	}
+	pos++;
       }else{
 	fprintf(stderr,"undefind char\n");
 	return;
       }
-      pos++;
       break;
     case s_number_int:
       if(ch == '.'){
@@ -144,7 +168,7 @@ void pass1(char *expr){
 	pos++;
 	num++;
       }else{
-	push("value","int",token,exprs);
+	push(VALUE,D_INT,token,exprs);
 	num=0;
 	memset(token,0,20);
 	stat = s_idle;
@@ -156,7 +180,7 @@ void pass1(char *expr){
 	num++;
 	pos++;
       }else{
-	push("value","float",token,exprs);
+	push(VALUE,D_FLOAT,token,exprs);
 	num=0;
 	stat = s_idle;
 	memset(token,0,20);
@@ -168,24 +192,24 @@ void pass1(char *expr){
 	num++;
 	pos++;
       }else if(ch == '.'){
-	push("value","name",token,exprs);
-	push("operator",NULL,&ch,exprs);
+	push(VALUE,D_NAME,token,exprs);
+	push(OPERATOR,0,&ch,exprs);
 	num=0;
 	pos++;
 	stat = s_idle;
 	memset(token,0,20);
       }else if(strstr("true false",token)){
-	push("value","bool",token,exprs);
+	push(VALUE,D_BOOL,token,exprs);
 	num=0;
 	stat = s_idle;
 	memset(token,0,20);
       }else if(strstr("in",token)){
-	push("operator",NULL,token,exprs);
+	push(OPERATOR,0,token,exprs);
 	num=0;
 	stat = s_idle;
 	memset(token,0,20);
       }else{
-	push("value","name",token,exprs);
+	push(VALUE,D_NAME,token,exprs);
 	num=0;
 	stat = s_idle;
 	memset(token,0,20);
@@ -196,3 +220,115 @@ void pass1(char *expr){
   }
 
 }
+void pass2(stack *output){
+  stack *p;
+  
+  stack *oper = init();
+  int p1,p2,dir;
+  struct op *_op;
+  while(exprs->next !=NULL){
+    p = pop(exprs);
+    if(p->type == LEFT_BRACE){
+      push_stack(p,oper);
+    }else if(p->type == RIGHT_BRACE){
+      while(1){
+	p = pop(oper);
+	if(p->type == LEFT_BRACE){
+	  break;
+	}
+	push_stack(p,output);
+      }
+    }else if(p->type == VALUE){
+      push_stack(p,output);
+    }else if(p->type == OPERATOR){
+      stack *o2 = oper->next;
+      if(o2 != NULL){
+	//	printf("p->token = %s\n",p->token);
+	_op = check_oper(p->token);
+	//	printf("[%s]\n",_op->ch);
+	p1 = _op->priority;
+	//	printf("p1 = %d\n",p1);
+	dir = _op->dir;
+	//	printf("o2->token = %s\n",o2->token);
+	_op = check_oper(o2->token);
+	//	printf("[%s]\n",_op->ch);
+	p2 = _op->priority;
+	//	printf("p2 = %d\n",p2);
+      }
+      while( o2!=NULL && o2->type == OPERATOR && ((p1 >= p2 && dir==LTR) || (p1 > p2 && dir==RTL)) ){
+	push_stack(pop(oper),oper);
+	show(output);
+	o2 = oper->next;
+	if(o2 != NULL){
+	  _op = check_oper(o2->token);
+	  p2 = _op->priority;
+	}	
+      }
+      push_stack(p,oper);
+     
+    }
+  }
+  while(oper->next != NULL){
+    p = oper->next;
+    if(p->type == LEFT_BRACE){
+      fprintf(stderr,"left_brace mismatch");
+      exit(0);
+    }
+    push_stack(pop(oper),output);
+  }
+  free_stack(oper);
+}
+void calc(stack *output){
+  stack *result = init();
+  stack *p;
+  int num;
+  stack *arg[2];
+  while(output->next != NULL){
+    p = pop(output);
+    switch(p->type){
+    case VALUE:
+      push_stack(p,result);
+      break;
+    case OPERATOR:
+      num = check_oper(p->token)->priority;
+      if(num <= (sizeof(result)/sizeof(stack)) ){
+	for(int i=0;i<num;i++){
+	  arg[i] = pop(result);
+	}
+	check_oper(p->token)->fun(arg);
+	
+      }else{
+	
+      }
+      
+      break;
+    }
+  }
+  
+}
+
+stack *calc_plus(stack **arg){
+  printf("++++++++++++++++\n");
+}
+stack *calc_sub(stack **arg){
+  
+}
+stack *calc_div(stack **arg){
+  
+}
+stack *calc_div_int(stack **arg){
+  
+}
+stack *calc_times(stack **arg){
+  
+}
+stack *calc_power(stack **arg){
+  
+}
+stack *calc_gt(stack **arg){
+  
+}
+stack *calc_lt(stack **arg){
+  
+}
+
--- calc.h
+++ calc.h
@@ -6,55 +6,89 @@
 #include<string.h>
 #include<malloc.h>
 #include<ctype.h>
+
+
 #define s_idle 1
-//#define s_string_double 2
-//#define s_string_single 3
 #define s_array 2
 #define s_number_int 3
 #define s_number_float 4
 #define s_name 5
 
+
+#define LEFT_BRACE 0
+#define RIGHT_BRACE 1
+#define OPERATOR 2
+#define VALUE 3
+
+#define D_INT 10
+#define D_FLOAT 11
+#define D_NAME 12
+#define D_BOOL 13
+
+#define LTR 2
+#define RTL 1
+
 char c_operator_prefix[] =  "!%^&*+()|></=-";
 
 typedef struct node{
-  char type[20];
-  char data_type[20];
+  int type;
+  int data_type;
   char token[30];
   struct node *next;
 }stack;
 
+
+stack *calc_plus(stack **arg);
+stack *calc_power(stack **arg);
+stack *calc_sub(stack **arg);
+stack *calc_div(stack **arg);
+stack *calc_div_int(stack **arg);
+stack *calc_times(stack **arg);
+stack *calc_gt(stack **arg);
+stack *calc_lt(stack **arg);
 struct op{
-  char ch[2];
-  int prority;
-  char dir[10];
+  char ch[3];
+  int priority;
+  int dir;
   int opers;
-  // stack *(*fun)(stack **arg);
-}oper[16]={
-  {"(", 1, NULL, 0},
-  {")", 1, NULL, 0},
-  {".", 1, "ltr", 2},
-  {"in",2, "ltr", 2},
-  {"*", 3, "ltr", 2},
-  {"/", 3, "ltr", 2},
-  {"%", 3, "ltr", 2},
-  {"^", 3, "rtl", 2},
-  {"+", 4, "ltr", 2},
-  {"-", 4, "ltr", 2},
-  {">", 6, "ltr", 2},
-  {"<", 6, "ltr", 2},
-  {">=",6, "ltr", 2},
-  {"<=",6, "ltr", 2},
-  {"==",7, "ltr", 2},
-  {"!=",7, "ltr", 2},
-  {"&&",11, "ltr", 2},
-  {"||",12, "ltr", 2}
+  stack *(*fun)(stack **);
+}oper[10]={
+  {"(", 1, 0, 0,NULL},
+  {")", 1, 0, 0,NULL},
+  {"*", 3, LTR, 2, calc_times},
+  {"/", 3, LTR, 2, calc_div},
+  {"%", 3, LTR, 2, calc_div_int},
+  {"^", 3, RTL, 2, calc_power},
+  {"+", 4, LTR, 2, calc_plus},
+  {"-", 4, LTR, 2, calc_sub},
+  {">", 6, LTR, 2, calc_gt},
+  {"<", 6, LTR, 2, calc_lt}
 };
+ /*
+   {".", 1, LTR, 2},
+   {"in",2, LTR, 2},
+   {">=",6, LTR, 2},
+   {"<=",6, LTR, 2},
+   {"==",7, LTR, 2},
+   {"!=",7, LTR, 2},
+   {"&&",11, LTR, 2},
+   {"||",12, LTR, 2}
+ */
+
+stack *init();
 void pass1(char *expr);
+void pass2(stack *output);
 void show(stack *head);
 void free_stack(stack *head);
 void add_list(stack *new,stack *head);
 void reverse(stack *head);
 stack *pop(stack *head);
-void push(char *type,char *data_type,char *token,stack *head);
-stack *init();
+void push(int type,int data_type,char *token,stack *head);
+void push_stack(stack *p,stack *head);
+struct op *check_oper(char *ch);
+
+
+
+
+
 #endif
